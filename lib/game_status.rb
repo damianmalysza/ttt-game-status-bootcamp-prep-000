116900466a0790706f45c8fab4320033e92a003e
Damian Malysza <damian.malysza@gmail.com>
2020-10-02 01:07:05 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,7 +16,7 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  if (board.all?{|i| i==" " || i == ""})
+  if (board.all?{|i| i== " " || i == ""})
     false
   else
     WIN_COMBINATIONS.each do |combo|
