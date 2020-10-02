0306daf1cb8452a400ef4df191249de576b8027f
Damian Malysza <damian.malysza@gmail.com>
2020-10-02 01:11:15 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -23,6 +23,8 @@ def won?(board)
       position_1 = combo[0]
       position_2 = combo[1]
       position_3 = combo[2]
+
+      if board[position_1] == "X" && board[position_2] == "X" && board[position_3] == "X"
   end
 end
 
