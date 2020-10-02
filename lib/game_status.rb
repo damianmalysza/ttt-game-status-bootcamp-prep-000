84dc7dea5575f0ed49f1e5b364e0ee3ad38bf6ae
Damian Malysza <damian.malysza@gmail.com>
2020-10-02 00:35:44 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,20 +16,7 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  WIN_COMBINATIONS.each do |win_combo|
-    position_1 = win_combo[0]
-    position_2 = win_combo[1]
-    position_3 = win_combo[2]
-    if board[position_1] == "X" && board[position_2] == "X" && board[position_3] == "X"
-      win_combo
-      break
-    elsif board[position_1] == "O" && board[position_2] == "O" && board[position_3] == "O"
-      win_combo
-      break
-    else
-      false
-    end
-  end
+
 end
 
 
