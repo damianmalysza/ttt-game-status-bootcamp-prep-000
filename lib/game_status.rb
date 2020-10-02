91b4b75204f961e5e3b74db17494ade2abdb7942
Damian Malysza <damian.malysza@gmail.com>
2020-10-02 00:57:00 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -22,6 +22,7 @@ def won?(board)
     WIN_COMBINATIONS.each do |combo|
       if (board[combo[0]] == "X" && board[combo[1]] == "X" && board[combo[2]] == "X")
         return combo
+      end
   end
 end
 
